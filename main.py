e669bf3a407ef4dbf3cc80c568d81cac1176c166
yashvid17-pixel <yashvid17@gmail.com>
2026-06-16 15:16:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-i in range (1,1001)
-print (i)
+num = int(input())
+
+if num < 0:
+    num = -num
+
+count = 0
+
+if num == 0:
+    count = 1
+else:
+    while num > 0:
+        count = count + 1
+        num = num // 10
+
+print(count)
